95d1620e103cf5a21ce2e42284d2046b1e49c97b
Daniel Vianna <dmlvianna@gmail.com>
2018-01-23 17:07:44 +1100

    reading csv with csv-conduit

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import qualified Data.ByteString  as B
+import           Data.CSV.Conduit
+import           Data.Text        (Text)
+import qualified Data.Vector      as V
+
+csvset :: Char -> CSVSettings
+csvset c = CSVSettings {csvSep = c, csvQuoteChar = Just '"'}
+
+readCsv :: String -> Char -> IO (V.Vector (Row Text))
+readCsv fp del = readCSVFile (csvset del) fp
+
+main :: IO (V.Vector (Row Text))
+main = readCsv "./data/pat_abstracts.csv" ','
