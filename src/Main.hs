ac0cbe9fa46a8cf0f67b65e9c738c6a0f8b2980a
Daniel Vianna <dmlvianna@gmail.com>
2018-01-23 17:23:54 +1100

    named records

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,34 @@
 
 module Main where
 
-import qualified Data.ByteString  as B
+import qualified Data.ByteString             as B
 import           Data.CSV.Conduit
-import           Data.Text        (Text)
-import qualified Data.Vector      as V
+import           Data.CSV.Conduit.Conversion
+import           Data.Text                   (Text)
+import qualified Data.Vector                 as V
 
 csvset :: Char -> CSVSettings
 csvset c = CSVSettings {csvSep = c, csvQuoteChar = Just '"'}
 
-readCsv :: String -> Char -> IO (V.Vector (Row Text))
+readCsv :: FilePath -> Char -> IO (V.Vector (Named Abstracts))
 readCsv fp del = readCSVFile (csvset del) fp
 
-main :: IO (V.Vector (Row Text))
+data Abstracts = Abstracts
+  { _id       :: !Integer
+  , _abstract :: !Text
+  } deriving (Show, Eq, Read)
+
+instance FromNamedRecord Abstracts where
+  parseNamedRecord m =
+    Abstracts <$>
+    m .: "AUSTRALIAN_APPL_NO" <*>
+    m .: "ABSTRACT_TEXT"
+
+instance ToNamedRecord Abstracts where
+  toNamedRecord (Abstracts _id _abstract) =
+    namedRecord [ "id" .= _id
+                , "abstract" .= _abstract
+                ]
+
+main :: IO (V.Vector (Named Abstracts))
 main = readCsv "./data/pat_abstracts.csv" ','
