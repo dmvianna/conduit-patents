68f39db29276a795c0531cd90929f63585dc73d4
Daniel Vianna <dmlvianna@gmail.com>
2018-02-06 10:28:55 +1100

    straight from ByteString to AuAddress

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,18 @@
 
 module Main where
 
-import qualified Data.ByteString             as B
+import           Control.Monad               (mzero)
+import qualified Data.ByteString.Lazy        as B
+import           Data.CaseInsensitive
 import           Data.CSV.Conduit
 import           Data.CSV.Conduit.Conversion
 import           Data.Text                   (Text)
+import qualified Data.Text                   as T
 import qualified Data.Vector                 as V
+import           Text.Trifecta
+
+import           Addresses
+import           Lower                       (lowercase)
 
 csvset :: Char -> CSVSettings
 csvset c = CSVSettings {csvSep = c, csvQuoteChar = Just '"'}
@@ -16,8 +23,18 @@ readCsv fp del = readCSVFile (csvset del) fp
 
 data Abstracts = Abstracts
   { _id       :: !Integer
-  , _abstract :: !Text
-  } deriving (Show, Eq, Read)
+  , _abstract :: !AuAddress
+  } deriving (Show, Eq)
+
+instance FromField AuAddress where
+  parseField s =
+    case parseByteString (step auAddress)
+         mempty (lowercase s) of
+      Success x -> pure x
+      Failure e -> mzero
+
+-- instance ToField AuAddress where
+
 
 instance FromNamedRecord Abstracts where
   parseNamedRecord m =
@@ -28,7 +45,7 @@ instance FromNamedRecord Abstracts where
 instance ToNamedRecord Abstracts where
   toNamedRecord (Abstracts _id _abstract) =
     namedRecord [ "id" .= _id
-                , "abstract" .= _abstract
+                -- , "abstract" .= _abstract
                 ]
 
 main :: IO (V.Vector (Named Abstracts))
